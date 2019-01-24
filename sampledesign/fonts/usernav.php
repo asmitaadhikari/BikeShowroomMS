a7f2fe2508a9dd09354d3c57f075a2ed201e69ec
asmitaadhikari <asmitaadhikari62@gmail.com>
2019-01-24 20:53:21 +0545

    sampledesign

---
+++ sampledesign/fonts/usernav.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+ 
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+ 
+  <script src="js/jquery.min.js"></script>
+  
+  <script src="js/popper.min.js"></script>
+
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Pastime Sports</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mx-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="userdashboard.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="gallery.php">Gallery</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contactus.php">Contact Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="events.php">Events</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="sport.php">Sports</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="posts.php">Forum</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="membershipfee.php">member</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="edituser.php">setting</a>
+      </li>
+     
+    
+    
+    </ul>
+    <form class="form-inline">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-light my-sm-0" type="submit">Search</button>
+      <a href="logout.php" class="btn btn-success my-sm-0">Logout</button></a>
+     
+    </form>
+  </div>
+</nav>
+</body>
+</html>
